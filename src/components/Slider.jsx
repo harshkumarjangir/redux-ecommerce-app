09fdb0279d1298575515ba1f47429c56b7f494aa
Harsh Kumar Jangir <harshkumarjangir2002@gmail.com>
2025-01-14 12:44:05 +0530

    redux ecommerce app code slider update

--- src/components/Slider.jsx
+++ src/components/Slider.jsx
@@ -3,25 +3,25 @@ import { MDBCarousel, MDBCarouselCaption, MDBCarouselItem } from 'mdb-react-ui-k
 
 const Slider = () => {
     return (
-        <div>
-            <MDBCarousel showControls showIndicators dark fade>
+        <div className=''>
+            <MDBCarousel showControls showIndicators fade>
                 <MDBCarouselItem itemId={1}>
                     <img src='/slider/slider1.png' className='d-block w-100' alt='...' />
-                    <MDBCarouselCaption>
+                    <MDBCarouselCaption className='rounded' style={{ background: "rgba(0,0,0,0.5)" }}>
                         <h5>Explore Cutting-Edge Tech</h5>
                         <p>Upgrade your lifestyle with the latest gadgets and accessories.</p>
                     </MDBCarouselCaption>
                 </MDBCarouselItem>
                 <MDBCarouselItem itemId={2}>
                     <img src='/slider/slider2.png' className='d-block w-100' alt='...' />
-                    <MDBCarouselCaption>
+                    <MDBCarouselCaption className='rounded' style={{ background: "rgba(0,0,0,0.5)" }}>
                         <h5>Transform Your Space</h5>
                         <p>Discover stylish home products designed for comfort and elegance.</p>
                     </MDBCarouselCaption>
                 </MDBCarouselItem>
                 <MDBCarouselItem itemId={3}>
                     <img src='/slider/slider3.png' className='d-block w-100' alt='...' />
-                    <MDBCarouselCaption>
+                    <MDBCarouselCaption className='rounded' style={{ background: "rgba(0,0,0,0.5)" }}>
                         <h5>Embrace Your Fitness Journey</h5>
                         <p>Stay active and healthy with premium fitness essentials.</p>
                     </MDBCarouselCaption>
